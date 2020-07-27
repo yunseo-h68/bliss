dcf4819d4ab3e21e4de14a5e7f33605410fad7e7
Yunseo Hwang <yunseo.h68@gmail.com>
2020-07-27 22:54:44 +0900

    Refactor few functions for version

--- src/version/version.c
+++ src/version/version.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <math.h>
 #include "version.h"
+#include "../util/util.h"
+
+struct version {
+	int release;
+	int major;
+	int minor;
+	char* str;
+
+	int (*get_release)(struct version* this);
+	int (*get_major)(struct version* this);
+	int (*get_minor)(struct version* this);
+	char* (*get_string)(struct version* this);
+	struct version* (*set_release)(struct version* this, int release);
+	struct version* (*set_major)(struct version* this, int major);
+	struct version* (*set_minor)(struct version* this, int minor);
+	struct version* (*set_version_from_str)(struct version* this, const char* str);
+	struct version* (*up_release)(struct version* this);
+	struct version* (*up_major)(struct version* this);
+	struct version* (*up_minor)(struct version* this);
+}
 
 static int version_parse_number(int* version_num, const char* version_str, int start, int finish)
 {
@@ -8,7 +28,7 @@ static int version_parse_number(int* version_num, const char* version_str, int s
 	long double version_temp = 0;
 
 	for (i = start; i < finish; i++) {
-		if ('0' > version[i] || '9' < version_str[i])
+		if ('0' > version_str[i] || '9' < version_str[i])
 			continue;
 		version_temp += (version_str[i] - '0') / pow(10, j);
 		j++;
@@ -17,35 +37,116 @@ static int version_parse_number(int* version_num, const char* version_str, int s
 	return i;
 }
 
-void version_print(const char* name, struct version_info version)
+static void version_parse(struct version* version_info, const char* str)
 {
-	printf("%s version %d.%d.%d\n",
-			name,
-			version.release,
-			version.major,
-			version.minor
-		  );
+	int finish = 0;
+	int start = 0;
+	int i = 0;
+	int *version_nums[3] = {&(version_info->release), &(version_info->major), &(version_info->minor)};
+
+	for (i = 0; i < 3; i++) {
+		while (str[finish+1] != '.' && str[finish+1]) 
+			finish++;
+		start = version_parse_number(version_nums[i], str, start, finish);
+		finish++;
+	}
 }
 
-void version_parse(struct version_info* version, const char* version_str)
+static char* version_to_string(struct version* version_info)
 {
-	int i = 0;
-	int j = 0;
+	int version_size = 
+		get_digit_number(version_info->release) + 
+		get_digit_number(version_info->major) + 
+		get_digit_number(version_info->minor) + 4;
+	char *version_str = (char*)malloc(sizeof(char) * version_size);
+	sprintf(version_str, "%d.%d.%d", version_info->release, version_info->major, version_info->minor);
+	return version_str;
+}
 
-	while (version_str[i++] != '.' && version_str[i]);
-	j = version_parse_number(&(version->release), version_str, j, i);
-	i++;
+static int get_release(struct version* this)
+{
+	return this->release;
+}
 
-	while (version_str[i++] != '.' && version_str[i]);
-	j = version_parse_number(&(version->major), version_str, j, i);
-	i++;
+static int get_major(struct version* this)
+{
+	return this->major;
+}
 
-	while (version_str[i++] != '.' && version_str[i]);
-	j = version_parse_number(&(version->minor), version_str, j, i);
+static int get_minor(struct version* this)
+{
+	return this->minor;
+}
 
-	if (version->release == 0 &&
-			version->major == 0 &&
-			version->minor == 0) {
-		version->minor++;
-	}
+static char* get_string(struct version* this)
+{
+	this->str = version_to_string(this);
+	return this->str;
+}
+
+static struct version* set_release(struct version* this, int release)
+{
+	this->release = release;
+	return this;
+}
+
+static struct version* set_major(struct version* this, int major)
+{
+	this->major = major;
+	return this;
+}
+
+static struct version* set_minor(struct version* this, int minor)
+{
+	this->minor = minor;
+	return this;
+}
+
+static struct version* set_version_from_str(struct version* this, const char* str)
+{
+	version_parse(this, str);
+	return this;
+}
+
+static struct version* up_release(struct version* this)
+{
+	(this->release)++;
+	return this;
+}
+
+static struct version* up_major(struct version* this)
+{
+	(this->major)++;
+	return this;
+}
+
+static struct version* up_minor(struct version* this)
+{
+	(this->minor)++;
+	return this;
+}
+
+struct version* new_version()
+{
+	struct version* tmp = (struct *version)malloc(sizeof(struct version));
+	tmp->get_release = get_release;
+	tmp->get_major = get_major;
+	tmp->get_minor = get_minor;
+	tmp->get_string = get_string;
+	tmp->set_release = set_release;
+	tmp->set_major = set_major;
+	tmp->set_minor = set_minor;
+	tmp->set_version_from_str = set_version_from_str;
+	tmp->up_release = up_release;
+	tmp->up_major = up_major;
+	tmp->up_minor = up_minor;
+	return tmp;
+}
+
+void delete_version(struct version* version_info)
+{
+	if (version_info->str != NULL)
+		free(version_info->str);
+	if (version_info != NULL)
+		free(version_info);
 }
--- src/version/version.h
+++ src/version/version.h
@@ -1,13 +1,21 @@
 #ifndef BLISS_VERSION_H
 #define BLISS_VERSION_H
 
-struct version_info {
-	int release;
-	int major;
-	int minor;
+struct version {
+	int (*get_release)(struct version* this);
+	int (*get_major)(struct version* this);
+	int (*get_minor)(struct version* this);
+	char* (*get_string)(struct version* this);
+	struct version* (*set_release)(struct version* this, int release);
+	struct version* (*set_major)(struct version* this, int major);
+	struct version* (*set_minor)(struct version* this, int minor);
+	struct version* (*set_version_from_str)(struct version* this, const char* str);
+	struct version* (*up_release)(struct version* this);
+	struct version* (*up_major)(struct version* this);
+	struct version* (*up_minor)(struct version* this);
 };
 
-void version_print(const char* name, struct version_info version);
-void version_parse(struct version_info *version, const char* version_str);
+struct version* new_version();
+void delete_version(struct version* version_info);
 
 #endif
